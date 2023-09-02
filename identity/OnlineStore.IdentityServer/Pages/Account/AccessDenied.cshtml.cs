fabd98e71944ce85cdd40b4b24e8d73ffb093659
Yaroslav Kovalov <yarik19022001@outlook.com>
2023-09-02 12:29:43 +0300

    Created identity server

---
+++ identity/OnlineStore.IdentityServer/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineStore.IdentityServer.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
